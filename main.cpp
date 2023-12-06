22f38b528c5f0a79412a413afd12e44ecc9b9bd5
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2023-12-05 20:44:58 -0330

    get keyboard input with SDL_GetKeyboardState

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // C++ standard
 #include <iostream>
-
+#include <typeinfo>
 // third party libs
 #include <SDL2/SDL.h>
 
@@ -14,7 +14,8 @@ int main(int argc, char* argv[]) {
     bool isGameRunning = true;
     // Event variable for event handling on main loop/sub loop
     SDL_Event event;
-
+    // Get the live state (which key is pressed) of the keyboard
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
 
     /* SOURCE CODE */
 
@@ -22,13 +23,13 @@ int main(int argc, char* argv[]) {
         std::cout<<"Not initialized!! \n"<<SDL_GetError()<< "\n";
     }else std::cout<<"SDL ready to go\n";
     
-
     window = SDL_CreateWindow("SDL Tutorial", 0, 0, 500, 500, SDL_WINDOW_SHOWN);
 
     if(window == NULL) {
         std::cout<<"failed to create window!"<<std::endl;
         return -1;
     }
+
     while(isGameRunning) {
         while(SDL_PollEvent(&event)) {
             switch(event.type) {
@@ -40,13 +41,22 @@ int main(int argc, char* argv[]) {
                     break;
                 case SDL_KEYDOWN:
                     std::cout<<"key has been pressed: ";
+                    // if you don't want a massive amount of if statements, use an inline function,
+                    // faster than normal functions, more readable than not using functions. btw scancode is an enum
                     if(event.key.keysym.scancode == SDL_SCANCODE_A) {
                         std::cout<<"Key A"<<std::endl;
                     }
-                    break; 
+                    break;
                 default:
                     std::cout<<"Unhandled event... What?"<<std::endl;
                     break;
+            }
+            // it seems like this could allow us to decouple the inputs from the other events. But, do we want to?
+
+            // Ok so Uint8 is literally just a string. however, trying to get a string out of it is impossible.
+            // because the values are 0 and 1, not ascii letters.
+            if(state[SDL_SCANCODE_RIGHT] == 1) {
+                std::cout<<"right key pressed"<<std::endl;
             }
         }
     }
